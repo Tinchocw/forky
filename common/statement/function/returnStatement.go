06817379cedd697c06455211314c373850342629
Máximo Gismondi <maximogismondi@gmail.com>
2025-11-18 19:16:05 +0000

    refactor: add nil check for ReturnStatement value in Print method

--- common/statement/function/returnStatement.go
+++ common/statement/function/returnStatement.go
@@ -12,9 +12,11 @@ type ReturnStatement struct {
 }
 
 func (r ReturnStatement) Print(start string) {
-	fmt.Printf("%s%s\n", start+string(common.LAST_CONNECTOR), common.Colorize("Value:", common.COLOR_YELLOW))
-	start += string(common.SIMPLE_INDENT) + string(common.LAST_CONNECTOR)
-	r.Value.Print(start)
+	if r.Value != nil {
+		fmt.Printf("%s%s\n", start+string(common.LAST_CONNECTOR), common.Colorize("Value:", common.COLOR_YELLOW))
+		start += string(common.SIMPLE_INDENT) + string(common.LAST_CONNECTOR)
+		r.Value.Print(start)
+	}
 }
 
 func (r ReturnStatement) Headline() string {
